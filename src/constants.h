b72b2c38adacd58c9aef99fe73d1106fb8a36ba5
Boyin Chen <boyinchen@tencent.com>
2021-12-12 16:18:22 +0800

    Add up version

--- src/constants.h
+++ src/constants.h
@@ -7,7 +7,7 @@
 // Version
 constexpr auto kVersionProject = "loadmaster";
 constexpr int kVersionMajor = 0;
-constexpr int kVersionMinor = 1;
+constexpr int kVersionMinor = 2;
 constexpr int kVersionPatch = 0;
 constexpr auto kVersionSuffix = "beta.0";
 
